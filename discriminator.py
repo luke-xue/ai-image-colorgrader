91e7a45e1cf58d0c926e5e52eef45212d32fbff6
Luke Xue <luqi9900@gmail.com>
2023-11-23 16:34:45 -0700

    generator

--- discriminator.py
+++ discriminator.py
@@ -1,6 +1,18 @@
 import torch
 import torch.nn as nn
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
+        super(CNNBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(
+                in_channels, out_channels, 4, stride, 1, bias=False, padding_mode="reflect"
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2),
+        )
 
+    def forward(self, x):
+        return self.conv(x)
 class Discriminator(nn.Module):
     def __init__(self, in_channels, features = [64,128,256,512]):
         super().__init__()
@@ -19,17 +31,7 @@ class Discriminator(nn.Module):
         in_channels = features[0]
         for feature in features[1:]:
             layers.append(
-                nn.Conv2d(
-                    in_channels,
-                    feature,
-                    kernel_size = 4,
-                    stride = 1 if feature == features[-1] else 2,
-                    padding = 1,
-                    bias=False,
-                    padding_mode="reflect",
-                ),
-                nn.BatchNorm2d(feature),
-                nn.LeakyReLU(0.2),
+                CNNBlock(in_channels, feature, stride=1 if feature == features[-1] else 2)
             )
             in_channels = feature
         layers.append(
@@ -44,3 +46,19 @@ class Discriminator(nn.Module):
         )
 
         self.model = nn.Sequential(*layers)
+
+    def forward(self, x, y):
+        x = torch.cat((x,y), dim=1)
+        x = self.initial(x)
+        return self.model(x)
+    
+def test():
+    x = torch.randn((1,3,256,256))
+    y = torch.randn((1,3,256,256))
+    model = Discriminator(3)
+    preds = model(x,y)
+    print(preds.shape)
+
+if __name__ == "__main__":
+    test()
+    
